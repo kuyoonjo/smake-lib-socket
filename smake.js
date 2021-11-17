83be8f5dcda8d4d69db51dedb54e31672fa87e6d
Yu Chen <yu.chen@live.ie>
2021-11-17 20:52:21 +0800

    v2.0.0

--- smake.js
+++ smake.js
@@ -1,60 +1,9 @@
-const { LLVM_Darwin, LLVM_Win32, LLVM_Linux } = require('smake');
-const { addLibs } = require('@smake/libs');
-const { buffer } = require('@smake/buffer');
-const { socket } = require('./lib');
+const { LLVM } = require('smake');
+const { LibSocket } = require('./lib');
 
-class darwin_arm64 extends LLVM_Darwin {
-  ARCH = 'arm64';
-  name = this.target;
-  includedirs = [
-    ...super.includedirs,
-    './include',
-  ];
-  cxflags = [
-    ...super.cxflags,
-    '-O3',
-    '-std=c++17',
-  ];
+const test = new LLVM('test', 'x86_64-linux-gnu');
+test.files = ['src/ex/socket.cc'];
+LibSocket.config(test);
+test.stdcxx = 'c++17';
 
-  files = ['src/ex/socket.cc'];
-}
-
-class linux_aarch64 extends LLVM_Linux {
-  target = 'aarch64-linux-gnu';
-  name = this.target;
-  includedirs = [
-    ...super.includedirs,
-    './include',
-  ];
-  cxflags = [
-    ...super.cxflags,
-    '-O3',
-    '-std=c++17',
-  ];
-
-  files = ['src/ex/socket.cc'];
-}
-
-class win32_x86_64 extends LLVM_Win32 {
-  ARCH = 'x86_64';
-  name = this.target;
-  includedirs = [
-    ...super.includedirs,
-    './include',
-  ];
-  cxflags = [
-    ...super.cxflags,
-    '-O3',
-    '-std=c++17',
-  ];
-
-  files = ['src/ex/socket.cc'];
-}
-
-module.exports = {
-  targets: [
-    addLibs(darwin_arm64, buffer, socket),
-    addLibs(linux_aarch64, buffer, socket),
-    addLibs(win32_x86_64, buffer, socket),
-  ],
-};
+module.exports = [test];
